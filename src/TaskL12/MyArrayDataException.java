3b71ea3914e1990c043f92afecb9992abc781de0
MaksimG193 <156239720+MaksimG193@users.noreply.github.com>
2024-06-18 18:52:42 +0300

    TaskL12

---
+++ src/TaskL12/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package TaskL12;
+
+public class MyArrayDataException  extends Exception{
+
+    public MyArrayDataException(String message){
+        super(message);
+    }
+}
---
+++ src/TaskL12/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package TaskL12;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
