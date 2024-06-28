7d2a504b428d94a663ae5b2dfff4cd5ecac7c642
MaksimG193 <156239720+MaksimG193@users.noreply.github.com>
2024-06-28 23:39:14 +0300

    TASKL15

---
+++ src/test/TestTaskl15.java
@@ -0,0 +1,49 @@
+package test;
+
+import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+
+import static java.awt.SystemColor.text;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestTaskl15 {
+
+    @Test
+    public void main() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://mts.by");
+
+        //1. Проверить название указанного блока
+        WebElement blockName = driver.findElement(By.xpath("//section[@class = 'pay']//h2"));
+        assertEquals("Онлайн пополнение" + "\n" + "без комиссии", blockName.getText());
+
+        //2. Проверить наличие логотипов платёжных систем
+        List<WebElement> payPartners = driver.findElements(By.xpath("//div[@class='pay__partners']"));
+        for(WebElement logo : payPartners) {
+            Assert.isTrue(
+                    (logo.getSize().width > 0 && logo.isDisplayed())
+                    , logo.getAttribute("alt") + " не отображается на странице"
+            );
+        }
+        //3. Проверить работу ссылки «Подробнее о сервисе»
+        WebElement clickServiceInfo = driver.findElement(By.xpath("//a[@href = '/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/']"));
+        Assert.assertEquals("Подробнее о сервисе" , clickServiceInfo.click());
+
+        //Заполнить поля и проверить работу кнопки «Продолжить» (проверяем только вариант «Услуги связи», номер для теста 297777777)
+        WebElement phoneNumberField = driver.findElement(By.id("phone_number"));
+        phoneNumberField.sendKeys("297777777");
+
+        WebElement continueButton = driver.findElement(By.xpath("//button[@class = 'button button__default ']"));
+        continueButton.click();
+    }
+
+    }
+
+
+
