3752e857097d54a1e6bd6e1ebfa74ee2f63cfd75
MaksimG193 <156239720+MaksimG193@users.noreply.github.com>
2024-07-13 15:48:37 +0300

    TASKL6.2

--- Java/Task13.java
+++
@@ -1,6 +0,0 @@
-public static void main() {
-    int[][] arr =new int[4][4];
-    for(int i= 0;i< arr.length; i++){
-        arr[i][i]= 1;
-    }
-}
--- Java/Task8.java
+++
@@ -1,9 +0,0 @@
-public static void main(){
-
-}
-public static void main(String[] args, int a){
-        for(int i = 0;i < a ;i++){
-            System.out.println("Element" + i);
-        }
-    }
-
