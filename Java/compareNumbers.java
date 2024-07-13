ec867e1dce5ef8702c83890d02bbb8d189df6fb1
MaksimG193 <156239720+MaksimG193@users.noreply.github.com>
2024-07-13 18:23:42 +0300

    TASKFactorialTestNG

--- Java/compareNumbers.java
+++
@@ -1,8 +0,0 @@
-public static void main() {
-    int a= 16; int b= 15;
-    if (a>=b){
-        System.out.println( "a>= b");
-    }else{
-        System.out.println("a< b");
-    }
-}
--- src/TaskL12/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package TaskL12;
-
-public class MyArrayDataException  extends Exception{
-
-    public MyArrayDataException(String message){
-        super(message);
-    }
-}
